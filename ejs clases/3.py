c59ce92c927bec553bd0c2f6116f1f9cea687a4f
fedeuhr <federicouhr@gmail.com>
2021-11-21 21:38:46 -0300

    Ej3 Terminado

--- ejs clases/3.py
+++ ejs clases/3.py
@@ -1,25 +1,65 @@
-class MatrizCalc:
-    def __init__(self, a, b, c, d, e, f, g, h, i):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-        self.e = e
-        self.f = f
-        self.g = g
-        self.h = h
-        self.i = i
+class Numero:
+    def __init__(self, numeroPrincipal=[], numeroSecundario=[]):
+        self.numeroPrincipal = numeroPrincipal
+        self.numeroSecundario = numeroSecundario
 
-    def imprimir(self):
-        def imprimirFilaA(self):
-            print("{}".format(self.a),"{}".format(self.b),"{}".format(self.c))
-        def imprimirFilaB(self):
-            print("{}".format(self.d),"{}".format(self.e),"{}".format(self.f))
-        def imprimirFilaC(self):
-            print("{}".format(self.g),"{}".format(self.h),"{}".format(self.i))   
-        imprimirFilaA(self)
-        imprimirFilaB(self)
-        imprimirFilaC(self)
-        
-matriz1 = MatrizCalc(1,2,3,4,5,6,7,8,9)
-matriz1.imprimir()
+    def __str__(self):
+        return "Su numero es {}".format(self.numeroPrincipal)
+
+    def suma(self):
+        resSuma = []
+        for i in range(len(self.numeroPrincipal)):
+            resSuma.append([])
+            for j in range(len(self.numeroPrincipal[0])):
+                resSuma[i].append(self.numeroPrincipal[i][j] + self.numeroSecundario[i][j])
+        print("Resultado Suma: ")
+        print(resSuma[0])
+        print(resSuma[1])
+        print(resSuma[2])
+        print('____________')
+
+    def resta(self):
+        resResta = []
+        for i in range(len(self.numeroPrincipal)):
+            resResta.append([])
+            for j in range(len(self.numeroPrincipal[0])):
+                resResta[i].append(self.numeroPrincipal[i][j] - self.numeroSecundario[i][j])
+        print("Resultado Resta: ")
+        print(resResta[0])
+        print(resResta[1])
+        print(resResta[2])
+        print('____________')
+    
+    def producto(self):
+        resProducto = []
+        for i in range(len(self.numeroPrincipal)):
+            resProducto.append([])
+            for j in range(len(self.numeroPrincipal[0])):
+                resProducto[i].append(self.numeroPrincipal[i][j] * self.numeroSecundario[i][j])
+        print("Resultado Producto: ")
+        print(resProducto[0])
+        print(resProducto[1])
+        print(resProducto[2])
+        print('____________')
+
+
+miMatriz = [[int(input("Dígito 1:1 : ")),int(input("Dígito 1:2 : ")),int(input("Dígito 1:3 : "))],
+            [int(input("Dígito 2:1 : ")),int(input("Dígito 2:2 : ")),int(input("Dígito 2:3 : "))],
+            [int(input("Dígito 3:1 : ")),int(input("Dígito 3:2 : ")),int(input("Dígito 3:3 : "))]]
+
+print(miMatriz[0])
+print(miMatriz[1])
+print(miMatriz[2])
+
+miMatrizDos = [[int(input("Dígito 1:1 : ")),int(input("Dígito 1:2 : ")),int(input("Dígito 1:3 : "))],
+            [int(input("Dígito 2:1 : ")),int(input("Dígito 2:2 : ")),int(input("Dígito 2:3 : "))],
+            [int(input("Dígito 3:1 : ")),int(input("Dígito 3:2 : ")),int(input("Dígito 3:3 : "))]]
+
+print(miMatrizDos[0])
+print(miMatrizDos[1])
+print(miMatrizDos[2])
+
+miNumeroUno = Numero(miMatriz, miMatrizDos)
+miNumeroUno.suma()
+miNumeroUno.resta()
+miNumeroUno.producto()
