c7f791649852dc63f30a6b31da50797b4183822e
Manuel López <manuel.lopez.csta@gmail.com>
2021-11-23 15:33:48 -0300

    ej2 sol

---
+++ ejs clases/2.py
@@ -0,0 +1,33 @@
+#Solucion Gise
+
+#Solucion Manu , Control+ k y control + c para block comment.
+# Para sacarlo, seleccionar el bloque y control + k , y luego control + u
+
+# class Vector:
+#     def __init__(self,x,y,z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+    
+#     def suma(self,escalar):
+#         newVector = (self.x+escalar,self.y+escalar,self.z+escalar)
+#         print("Resultado Suma:",newVector)
+
+#     def resta(self,escalar):
+#         newVector = (self.x-escalar,self.y-escalar,self.z-escalar)
+#         print("Resultado Resta:",newVector)
+
+#     def multi(self,escalar):
+#         newVector = (self.x*escalar,self.y*escalar,self.z*escalar)
+#         print("Resultado Multi:",newVector)
+
+#     def divi(self,escalar):
+#         newVector = (self.x/escalar,self.y/escalar,self.z/escalar)
+#         print("Resultado Divi:",newVector)
+    
+
+# num1 = Vector(2,2,2)
+# num1.suma(5)
+# num1.resta(1)
+# num1.multi(2)
+# num1.divi(2)
