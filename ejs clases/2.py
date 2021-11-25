b15a6c3317bae5e38c8bf3b92a32ce2230c28562
Manuel López <manuel.lopez.csta@gmail.com>
2021-11-25 17:17:27 -0300

    fix merge ej 2

--- ejs clases/2.py
+++ ejs clases/2.py
@@ -1,3 +1,4 @@
+#Solucion Gise
 class Vector3D:
     def __init__(self, n1, n2, n3):
         self.n1 = n1
@@ -46,6 +47,39 @@ vector.restar(vector2)
 vector.multiplicar(2)
 
 vector.dividir(2)
+
+#Solucion Manu , Control+ k y control + c para block comment.
+# Para sacarlo, seleccionar el bloque y control + k , y luego control + u
+
+# class Vector:
+#     def __init__(self,x,y,z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+    
+#     def suma(self,escalar):
+#         newVector = (self.x+escalar,self.y+escalar,self.z+escalar)
+#         print("Resultado Suma:",newVector)
+
+#     def resta(self,escalar):
+#         newVector = (self.x-escalar,self.y-escalar,self.z-escalar)
+#         print("Resultado Resta:",newVector)
+
+#     def multi(self,escalar):
+#         newVector = (self.x*escalar,self.y*escalar,self.z*escalar)
+#         print("Resultado Multi:",newVector)
+
+#     def divi(self,escalar):
+#         newVector = (self.x/escalar,self.y/escalar,self.z/escalar)
+#         print("Resultado Divi:",newVector)
+    
+
+# num1 = Vector(2,2,2)
+# num1.suma(5)
+# num1.resta(1)
+# num1.multi(2)
+# num1.divi(2)
+
 
 
 
