fcf8a81f380f3392f65fd7ec443b5564db8d550a
GiseGonzalez <fije.gonzalez@gmail.com>
2021-11-24 21:45:05 -0300

    Revert "Merge branch 'main' of https://github.com/CaC-Grupo-9/backend"
    This reverts commit 55b8f9c849667e6b59e83116721dd1a3e179d8be, reversing
    changes made to 32f08e193fd26b9c8b79d3b6aaadf84c6def3409.

--- ejs clases/2.py
+++ ejs clases/2.py
@@ -1,33 +1,51 @@
-#Solucion Gise
+class Vector3D:
+    def __init__(self, n1, n2, n3):
+        self.n1 = n1
+        self.n2 = n2
+        self.n3 = n3
 
-#Solucion Manu , Control+ k y control + c para block comment.
-# Para sacarlo, seleccionar el bloque y control + k , y luego control + u
+    def __str__(self):
+         return "(" f"{self.n1} {self.n2} {self.n3}" ")"
 
-# class Vector:
-#     def __init__(self,x,y,z):
-#         self.x = x
-#         self.y = y
-#         self.z = z
-    
-#     def suma(self,escalar):
-#         newVector = (self.x+escalar,self.y+escalar,self.z+escalar)
-#         print("Resultado Suma:",newVector)
+    def sumar(self,vector):
+        self.n1 = (self.n1 + vector.n1) 
+        self.n2 = (self.n2 + vector.n2) 
+        self.n3 = (self.n3 + vector.n3)
+        print(self)
 
-#     def resta(self,escalar):
-#         newVector = (self.x-escalar,self.y-escalar,self.z-escalar)
-#         print("Resultado Resta:",newVector)
+    def restar(self, vector):
+        self.n1 = (self.n1 - vector.n1) 
+        self.n2 = (self.n2 - vector.n2) 
+        self.n3 = (self.n3 - vector.n3)
+        print(self)
 
-#     def multi(self,escalar):
-#         newVector = (self.x*escalar,self.y*escalar,self.z*escalar)
-#         print("Resultado Multi:",newVector)
+    def multiplicar(self, escalar):
+        self.n1 = (self.n1 * escalar) 
+        self.n2 = (self.n2 * escalar) 
+        self.n3 = (self.n3 * escalar)
+        print(self)
 
-#     def divi(self,escalar):
-#         newVector = (self.x/escalar,self.y/escalar,self.z/escalar)
-#         print("Resultado Divi:",newVector)
-    
+    def dividir(self, escalar):
+        self.n1 = (self.n1 / escalar) 
+        self.n2 = (self.n2 / escalar) 
+        self.n3 = (self.n3 / escalar)
+        print(self)
+          
 
-# num1 = Vector(2,2,2)
-# num1.suma(5)
-# num1.resta(1)
-# num1.multi(2)
-# num1.divi(2)
+vector = Vector3D(1,3,1)
+vector2 = Vector3D(4,7,2)
+
+print(vector)
+print(vector2)
+
+
+vector.sumar(vector2)
+
+vector.restar(vector2)
+
+vector.multiplicar(2)
+
+vector.dividir(2)
+
+
+
